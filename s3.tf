541b8e570c0933742684e473d4849edaafa2bc0b
Satish Kumar Mandoddi <satishkumar432@gmail.com>
2019-01-04 06:57:33 -0700

    tags, locals and multiple buckets

--- s3.tf
+++ s3.tf
@@ -2,17 +2,12 @@ resource "aws_s3_bucket" "main" {
   bucket = "my-432-tf-test-bucket"
   acl    = "private"
 
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
+  tags = "${local.s3_tags}"
+  region = "${var.s3_regions[0]}"
 }
 resource "aws_s3_bucket" "child-1" {
   bucket = "my-4312-tf-test-bucket"
   acl    = "private"
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
+  tags = "${local.s3_tags}"
+  region = "${var.s3_regions[0]}"
 }
--- variables.tf
+++ variables.tf
@@ -1,18 +1,33 @@
 variable "s3_bucket_name" {
-  #default = "my-432-tf-test-bucket"
+  default = "my-432-tf-test-bucket"
   description = "This is my default bucket name"
   type = "string"
 }
 
-variable "s3_tags" {
+variable "environment" {
+  default = "test"
+  description = "Holds the environment_variable"
+  type = "string"
+}
+
+locals{
+  s3_tags = {
+    created_by = "terraform"
+    environment = "${var.environment}"
+  }
+}
+
+/*variable "s3_tags" {
   type = "map"
   default = {
     created_by = "terraform"
     environment ="test"
   }
-}
+}*/
+
+
 
 variable "s3_regions" {
   type = "list"
-  default = ["us-west-2"]
+  default = ["us-east-1"]
 }
