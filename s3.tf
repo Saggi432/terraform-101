46113db7b704f9b595e41d3448d8dcc78d51e54b
Satish Kumar Mandoddi <satishkumar432@gmail.com>
2019-01-04 06:28:34 -0700

    new changes

--- s3.tf
+++ s3.tf
@@ -7,3 +7,12 @@ resource "aws_s3_bucket" "main" {
     Environment = "Dev"
   }
 }
+resource "aws_s3_bucket" "child-1" {
+  bucket = "my-4312-tf-test-bucket"
+  acl    = "private"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
